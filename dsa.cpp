c7ce18405114ae62c581dbb96d953b4b3d2325e1
Pritam <pritam18jan@gmail.com>
2024-09-16 22:30:41 +0530

    Brute force ! Return pari in sorted array with target sum. nums = [2,7,11,15]

--- dsa.cpp
+++ dsa.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+// brute force
+vector<int> pairSum(vector<int> nums, int target){
+  vector<int> ans;
+  int n = nums.size();
 
-int binToDecimal(int binNum){
-int ans = 0, pow = 1;
-
-while(binNum > 0){
-  int rem = binNum % 2;
-   ans += rem * pow;
-
-   binNum /= 10;
-   pow *= 2;
-}
-return ans;
+   for(int i= 0; i<n; i++){
+    for(int j= i+1; j<n; j++){
+      if(nums[i] + nums[j] == target){
+        ans.push_back(i);
+        ans.push_back(j);
+        return ans;
+      }
+    }
+  }
+  return ans;
 }
 
 int main(){
-  cout << binToDecimal(1010) << endl;
+  vector<int> nums = {2, 7, 11, 15};
+  int target = 13;
+
+  vector<int> ans = pairSum(nums, target);
+  cout << ans[0] << "," << ans[1] << endl;
   return 0;
 }
