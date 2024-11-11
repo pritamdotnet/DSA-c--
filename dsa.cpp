23397f8835523dccbf03c2c437ff74f88a1909b9
Pritam <pritam18jan@gmail.com>
2024-11-11 18:00:40 +0530

    Painter's Partition Problem | DSA Placement Series - Binary Search | Lecture 22

--- dsa.cpp
+++ dsa.cpp
@@ -1,57 +1,55 @@
 #include <iostream>
 #include <vector>
+#include <climits> 
 using namespace std;
-// O(n)
 
-bool isValid(vector<int> &arr, int n, int m, int maxAllowedPages){
-  int students=1, pages=0;
 
+bool isPossible(vector<int> &arr, int n, int m, int maxAllowedTime){
+    int painters = 1, time = 0;
+
+    for(int i=0; i<n; i++){
+      if(time + arr[i] <= maxAllowedTime){
+          time += arr[i];
+      } else {
+        painters++;
+        time = arr[i];
+      }
+    }
+
+    return painters <= m ;
+
+}
+
+int minTimeToPaint(vector<int> &arr, int n, int m ){
+  int sum = 0, maxVal = INT_MIN;
   for(int i=0; i<n; i++){
-    if(arr[i] > maxAllowedPages){
-      return false;
-    }
+      sum += arr[i];
+      maxVal = max(maxVal, arr[i]);
+ }
 
-    if(pages + arr[i] <= maxAllowedPages){
-      pages += arr[i];
-    } else {
-        students++;
-        pages = arr[i];
+  int st = maxVal, end = sum, ans = -1;
+
+  while(st <= end) {
+    int mid = st + (end-st)/2;
+
+    if(isPossible(arr, n, m, mid)) { // left
+      ans = mid;
+      end = mid - 1;
+    } else { // right
+      st = mid + 1;
     }
   }
-  return students > m ? false : true;
-}
 
-int allocateBooks(vector<int> &arr, int n, int m ){
-  if(m>n){
-    return -1;
-  }
-  
-int sum = 0;
-for(int i =0; i<n; i++){
-  sum += arr[i];
-}
+  return ans;
 
-int ans = -1;
-int st = 0, end = sum; // range of possible ans
 
-while(st <= end){
-  int mid = st + (end-st)/2;
 
-  if(isValid(arr,n,m,mid)){ // left 
-    ans = mid;
-    end = mid-1;
-} else { // right
-    st = mid +1;
-}
-
-}
-return ans;
 }
 
 int main(){
-  vector<int> arr = {2, 1, 3, 4};
+  vector<int> arr = {40, 30, 10, 20};
  int n = 4,  m = 2;
 
-  cout << allocateBooks(arr, n, m)  << endl;
+  cout << minTimeToPaint(arr, n, m)  << endl;
   return 0;
 }
