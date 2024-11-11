c30a4ecf42ac5dfb16456d9e90ea36875b8ace06
Pritam <pritam18jan@gmail.com>
2024-11-11 16:47:45 +0530

    Book Allocation or Allocate books proble lecture 21

--- dsa.cpp
+++ dsa.cpp
@@ -3,32 +3,55 @@
 using namespace std;
 // O(n)
 
-vector<int> pairSum(vector<int> nums, int target){
-  vector<int> ans;
-  int n = nums.size();
+bool isValid(vector<int> &arr, int n, int m, int maxAllowedPages){
+  int students=1, pages=0;
 
-int i = 0, j = n-1;
+  for(int i=0; i<n; i++){
+    if(arr[i] > maxAllowedPages){
+      return false;
+    }
 
-while(i < j){
-  int pairSum = nums[i] + nums[j];
-  if(pairSum > target){
-    j--;
-  }else if(pairSum < target){
-    i++;
-  }else {
-    ans.push_back(i);
-    ans.push_back(j);
-    return ans;
+    if(pages + arr[i] <= maxAllowedPages){
+      pages += arr[i];
+    } else {
+        students++;
+        pages = arr[i];
+    }
   }
+  return students > m ? false : true;
 }
-  return ans;
+
+int allocateBooks(vector<int> &arr, int n, int m ){
+  if(m>n){
+    return -1;
+  }
+  
+int sum = 0;
+for(int i =0; i<n; i++){
+  sum += arr[i];
+}
+
+int ans = -1;
+int st = 0, end = sum; // range of possible ans
+
+while(st <= end){
+  int mid = st + (end-st)/2;
+
+  if(isValid(arr,n,m,mid)){ // left 
+    ans = mid;
+    end = mid-1;
+} else { // right
+    st = mid +1;
+}
+
+}
+return ans;
 }
 
 int main(){
-  vector<int> nums = {2, 7, 11, 15};
-  int target = 9;
+  vector<int> arr = {2, 1, 3, 4};
+ int n = 4,  m = 2;
 
-  vector<int> ans = pairSum(nums, target);
-  cout << ans[0] << "," << ans[1] << endl;
+  cout << allocateBooks(arr, n, m)  << endl;
   return 0;
 }
